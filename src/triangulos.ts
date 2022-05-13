f67b671271ab8dfa0f0fab0234a48a0c4132044b
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-13 18:25:29 -0300

    Initial commit

---
+++ src/triangulos.ts
@@ -0,0 +1,31 @@
+// Modularización y Métodos -CLASE 6- viernes 6 de mayo
+// MÉTODOS
+//Ejercicio ONLINE: Triángulos
+
+/* 
+• Realice un programa que devuelva el área del triángulo usando los siguientes pares de base-altura:
+• (1,2) (2,4) (3,6) (4,8) (5, 10) (6,12) (7,14)
+• Implemente un método llamado calcularAreaTriangulo que reciba dos números por parámetro (uno llamado base y otro altura)
+*/
+
+let titulo1 = document.getElementById("titulo1");
+let titulo2 = document.getElementById("titulo2");
+let numero1 = document.getElementById("numero1");
+let numero2 = document.getElementById("numero2");
+let btnCalcular = document.getElementById("calcular");
+
+titulo1.innerHTML = "Ingrese base";
+titulo2.innerHTML = "Ingrese altura";
+
+btnCalcular.addEventListener("click", () => {
+  //let dato1: number = Number(numero1.value);
+  //let dato2: number = Number(numero2.value);  ----> DESCOMENTAR PARA INGRESO DE DATOS
+
+  let calcularAreaTriangulo = (base: number, altura: number): number => {
+    return (base * altura) / 2;
+  };
+
+  for (let i: number = 1; i <= 7; i++) {
+    console.log("El area del triangulo", calcularAreaTriangulo(i, i * 2));
+  }
+});
